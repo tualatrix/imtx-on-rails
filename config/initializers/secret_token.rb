3c01e7d30f587d2f0958b8e6b0d0c562840bbdd8
Tualatrix Chou <tualatrix@gmail.com>
2012-01-13 15:50:45 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImtxOnRails::Application.config.secret_token = '750063e9e9dee727f8e5c5a2a302a1cf33e2f081a98444db364ceb7433e0fc77e85567da195a5790adbe4bbbc3fc18992c8d551feda7eb7a88a68355a2faf2e4'
