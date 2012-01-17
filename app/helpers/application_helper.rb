e7c78b329a4b96a1c9d3ee18a612b39ce62e29d5
Tualatrix Chou <tualatrix@gmail.com>
2012-01-17 19:44:05 +0800

    OK, this time really set the current home style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,21 +1,27 @@
 module ApplicationHelper
   def menus(request)
     menu_messages = []
-    if request.path_info == '/'
-      menu_messages << '<li class="current_page_item"><a href="/" title="Home">Home</a></li>'
-    else
-      menu_messages << '<li class="page_item"><a href="/" title="Home">Home</a></li>'
-    end
+    has_set_current = false
 
     Post.where('post_type = "page" AND status = "publish"').each do |page|
       title_path = "/#{page.title}"
-      text = content_tag(:li, link_to(page.title, title_path), :class => if request.path_info == title_path
-                'current_page_item'
-              else
-                'page_item'
-              end)
+      text = content_tag(:li,
+                         link_to(page.title, title_path),
+                         :class => if request.path_info == title_path
+                                     has_set_current = true
+                                     'current_page_item'
+                                   else
+                                     'page_item'
+                                   end)
       menu_messages << text
     end
-    menu_messages.join("\n").html_safe
+
+    home_style = if has_set_current
+                   'page_item'
+                 else
+                   'current_page_item'
+                 end
+
+    ["<li class='#{home_style}'><a href='/' title='Home'>Home</a></li>"].concat(menu_messages).join("\n").html_safe
   end
 end
