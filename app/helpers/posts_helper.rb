adbe8bb6bd152ab751acb7825ae1f7562457eca9
Tualatrix Chou <tualatrix@gmail.com>
2012-01-17 22:02:59 +0800

    Recent_posts for sidebar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+  def recent_posts
+    html_text = []
+
+    Post.where('post_type = "post" AND status = "publish"').order('date DESC').each do |post|
+          html_text << content_tag(:li, link_to(post.title, post))
+    end
+    html_text.join("\n").html_safe
+  end
 end
